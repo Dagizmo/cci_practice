ef19b66464e42e2c0d35ac771cf9d05edd78ea4a
Jose de Jesus Medina Rios <dgizmo6@gmail.com>
2018-09-25 18:34:40 -0700

    1.3 solution

---
+++ Code_practice/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPermutation(string a, string b)
+{
+	if (a.size() != b.size())
+	{
+		return false;
+	}
+
+	char countA[256] = { 0 };
+	char countB[256] = { 0 };
+
+	for (int i = 0; i < a.size(); ++i)
+	{
+		countA[a[i]]++;
+		countB[b[i]]++;
+	}
+
+	for (int i = 0; i < 256; ++i)
+	{
+		if (countA[i] != countB[i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	string a = "HeloA";
+	string b = "ellHo";
+	cout << isPermutation(a,b) << endl;
+	system("pause");
+}
